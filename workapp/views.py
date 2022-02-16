c7c778199e513fe0c2314d4c6c709da82d6bd825
power080900 <power080900@gmail.com>
2022-02-16 15:17:07 +0900

    1

--- workapp/views.py
+++ workapp/views.py
@@ -3,14 +3,9 @@ from django.shortcuts import render
 import datetime
 
 # Create your views here.
-
-
-
 def exercise1(request):
     today = datetime.date.today()
     m = today.month
     d = today.day
-    month = request.GET.get('month',m)
-    date = request.GET.get('date',d)
-    context = {'month':month, 'date':date}
+    context = {'month':m, 'date':d}
     return render(request,'exercise1.html',context)
